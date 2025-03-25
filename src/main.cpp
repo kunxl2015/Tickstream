082c94409f410c0aa697d40260a7affa6089e29d
kunxl-gg <tiwari.25@iitj.ac.in>
2025-03-26 00:54:11 +0530

    fix: add command line arguments to the program
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
-#include "engine.h"
-#include "marketData.hpp"
-#include <iostream>
+#include "pipeline.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		std::cerr << "Usage: " << argv[0] << "<inputDir> <outputDir>\n";
+		return 1;
+	}
+
+	const char *inputDir = argv[1];
+	const char *outputDir = argv[2];
+
+	std::cout << "Program name: " << argv[0] << std::endl;
+	std::cout << "InputDir: " << inputDir << std::endl;
+	std::cout << "OutputDir: " << outputDir << std::endl;
+
+	// Initialise and start the pipeline
+	tickstream::Pipeline pipeline(inputDir, outputDir);
+	pipeline.init();
+	pipeline.run();
+	pipeline.shutdown();
+
 	return 0;
 }
